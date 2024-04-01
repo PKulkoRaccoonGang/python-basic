0321eba651a29c770bf441a951b4d7cc597ee658
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2024-04-01 10:20:50 +0300

    hw-8: Fixed bug

--- hw-8.py
+++ hw-8.py
@@ -3,14 +3,14 @@
 # [6] => 36
 # [] => 0
 
-# list_of_numbers = [0, 1, 7, 2, 4, 8]
+list_of_numbers = [0, 1, 7, 2, 4, 8]
 # list_of_numbers = [1, 3, 5]
 # list_of_numbers = [6]
-list_of_numbers = []
+# list_of_numbers = []
 
 if list_of_numbers:
     sum_even_idx = sum(list_of_numbers[::2])
     result = sum_even_idx * list_of_numbers[-1]
     print(f'Result: {result}')
 else:
-    print(f'Result: {list_of_numbers}')
+    print(f'Result: {len(list_of_numbers)}')
